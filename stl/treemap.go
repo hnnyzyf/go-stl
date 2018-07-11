cdc6a8aea5a21a78f772e0aae405a7ae250a3cb6
hnnyzyf <hnnyzyf2013@163.com>
2018-07-12 00:54:56 +0800

    add treemap and entry

--- stl/treemap.go
+++ stl/treemap.go
@@ -1,7 +1,64 @@
 package stl
 
-type Entry interface {
+//TreeMap is a RBTree
+type TreeMap struct {
+	r *RBTree
 }
 
-type TreeMap struct {
+func NewTreeMap() *TreeMap {
+	return &TreeMap{
+		r: NewRBTree(),
+	}
+}
+
+//Put will add a key into TreeMap if  not exist or update a key if exists
+func (t *TreeMap) Put(e Entry) {
+	//do only once
+	t.r.lazyInit(e.(Value))
+
+	//add a new node success
+	if n, ok := t.r.add(e.(Value)); ok {
+		t.r.size++
+		t.r.balance(n)
+	} else {
+		n.val = e.(Value)
+	}
+
+}
+
+//Delete will delete a key if exists or do nothing
+func (t *TreeMap) Delete(e Entry) {
+	t.r.Pop(e.(Value))
+}
+
+//Get will return value if exists a key or nil if  not exist
+func (t *TreeMap) Get(e Entry) (interface{}, bool) {
+	//find a key
+	if n := t.r.find(e.(Value)); n != nil {
+		return n.val.(Entry).GetValue(), true
+	} else {
+		return nil, false
+	}
+}
+
+//Get will return value if exists a key or nil if  not exist
+func (t *TreeMap) Iterator(isAsc bool) func() (Entry, bool) {
+	if isAsc {
+		next := t.r.AscIter()
+		return func() (Entry, bool) {
+			n, ok := next()
+			return n.(Entry), ok
+		}
+	} else {
+		next := t.r.DescIter()
+		return func() (Entry, bool) {
+			n, ok := next()
+			return n.(Entry), ok
+		}
+	}
+}
+
+//Len return the size of rbtree
+func (t *TreeMap) Len() int {
+	return t.r.Len()
 }
---
+++ stl/treemap_test.go
@@ -0,0 +1,166 @@
+package stl
+
+import (
+	"testing"
+)
+
+type kv struct {
+	k int
+	v int
+}
+
+func new(k int, v int) *kv {
+	return &kv{k, v}
+}
+
+func (k *kv) Less(v Value) bool {
+	return k.k < v.(*kv).k
+}
+
+func (k *kv) Equal(v Value) bool {
+	return k.k == v.(*kv).k
+}
+
+func (k *kv) More(v Value) bool {
+	return k.k > v.(*kv).k
+}
+
+func (k *kv) GetKey() interface{} {
+	return k.k
+}
+
+func (k *kv) GetValue() interface{} {
+	return k.v
+}
+
+func Test_Put(t *testing.T) {
+	tree := NewTreeMap()
+	a := []*kv{
+		{1, 0},
+		{2, 0},
+		{3, 0},
+		{4, 0},
+		{4, 1},
+		{4, 2},
+		{2, 1},
+	}
+	res := []*kv{
+		{1, 0},
+		{2, 1},
+		{3, 0},
+		{4, 2},
+	}
+
+	for i := range a {
+		tree.Put(a[i])
+	}
+
+	next := tree.Iterator(true)
+
+	for i := range res {
+		n, ok := next()
+		k := n.GetKey().(int)
+		v := n.GetValue().(int)
+		if k != res[i].k || v != res[i].v || !ok {
+			t.Error("Fail,Expect ", res[i].k, ":", res[i].v, "(", k, ":", v, ")")
+		}
+	}
+
+}
+
+func Test_Delete(t *testing.T) {
+	tree := NewTreeMap()
+	a := []*kv{
+		{1, 0},
+		{2, 0},
+		{3, 0},
+		{4, 0},
+		{4, 1},
+		{4, 2},
+		{2, 1},
+	}
+
+	del := []*kv{
+		{3, 0},
+		{3, 0},
+		{4, 1},
+	}
+
+	res := []*kv{
+		{1, 0},
+		{2, 1},
+	}
+
+	for i := range a {
+		tree.Put(a[i])
+	}
+
+	for i := range del {
+		tree.Delete(del[i])
+	}
+
+	next := tree.Iterator(true)
+
+	for i := range res {
+		n, ok := next()
+		k := n.GetKey().(int)
+		v := n.GetValue().(int)
+		if k != res[i].k || v != res[i].v || !ok {
+			t.Error("Fail,Expect ", res[i].k, ":", res[i].v, "(", k, ":", v, ")")
+		}
+	}
+
+}
+
+func Test_Get(t *testing.T) {
+	tree := NewTreeMap()
+	a := []*kv{
+		{1, 0},
+		{2, 0},
+		{3, 0},
+		{4, 0},
+		{4, 1},
+		{4, 2},
+		{2, 1},
+	}
+
+	res := []*kv{
+		{1, 0},
+		{2, 1},
+		{3, 0},
+		{4, 2},
+	}
+
+	for i := range a {
+		tree.Put(a[i])
+	}
+
+	for i := range res {
+		v, ok := tree.Get(res[i])
+
+		if !ok || v.(int) != res[i].v {
+			t.Error("Fail,Expect ", res[i].v, "(", v, ")")
+		}
+	}
+}
+
+func BenchmarkPut(b *testing.B) {
+	t := NewTreeMap()
+	for i := 0; i < b.N; i++ {
+		e := new(i, i)
+		t.Put(e)
+	}
+}
+
+func BenchmarkGet(b *testing.B) {
+	t := NewTreeMap()
+	for i := 0; i < 1000; i++ {
+		e := new(i, i)
+		t.Put(e)
+	}
+
+	for i := 0; i < b.N; i++ {
+		e := new(i, i)
+		t.Get(e)
+	}
+}
